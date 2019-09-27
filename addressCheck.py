40e9324d987278ea4fd792bfe23979c976cdab21
Chris Lee <37919112+chrisl0411@users.noreply.github.com>
2019-09-27 16:26:53 -0500

    Version 2.0

--- addressCheck.py
+++ addressCheck.py
@@ -4,16 +4,20 @@ import pandas as pd
 import pgeocode
 import xlwings as xw
 
+# sheetVar: excel sheet variable; excelParam: value imported from excel; geoParam: value imported from pgeocode; columnLetter; row index
+# if else statement that checks excel value against pgeocode values
 def checkValue(sheetVar, excelParam, geoParam, columnLetter, row):
     if excelParam == geoParam:
         sheetVar.range(columnLetter+str(row)).value = "TRUE"
     else:
         sheetVar.range(columnLetter+str(row)).value = geoParam
 
+# function that allows for a one at a time check
 def pgeocodeCheck(countryCode,postCode):
     postal = pgeocode.Nominatim(countryCode)
     print(postal.query_postal_code(str(postCode)))
 
+# script runs through excel sheet 
 def cleanAddresses():
     data = pd.read_excel("cleanaddresses.xlsx",
                         sheet_name="Clean Addresses Test")
@@ -37,11 +41,11 @@ def cleanAddresses():
                 # check pgeocode data to excel data
                 postal = pgeocode.Nominatim(countryCode)
                 query = postal.query_postal_code(str(postCode))
-                # checks state name
+                # checks state name, at column "H"; change to desired column
                 checkValue(addresses, state, query['state_name'], 'H',row)
-                # checks city name
+                # checks city name, at column "J"; change to desired column
                 checkValue(addresses, city, query['place_name'], 'J',row)
-                # checks postal code
+                # checks postal code, at column "L"; change to desired column
                 checkValue(addresses, postCode, query['postal_code'], 'L',row)
 
             except Exception:
@@ -49,7 +53,11 @@ def cleanAddresses():
                 continue
 
 def main():
-    cleanAddresses()
-    #pgeocodeCheck("GB","NP10 8FZ")
+    #runs clean address function
+    #cleanAddresses()
+
+    #one by one check of address info with country and postal code inputs
+    pgeocodeCheck("MY",
+                "40400")
 
 main()
